4d5666c65c97dde069682d37799c76ba84c8d462
Ahmed Essam <eng.eramax@gmail.com>
2011-07-02 14:35:08 +0300

    comments running well from internal users and external users

--- application/views/comment_form.php
+++ application/views/comment_form.php
@@ -4,22 +4,25 @@ foreach ($comments as $comment) {
     $ccc = $ccc. $comment->username . '</br> םÞזב : ';
 	$ccc = $ccc. $comment->text . '</br></br>';
 }
+echo $ccc;
 ?>
 
 
 		<?php
-			echo $ccc;
 			$this->load->helper('form');
 			echo form_open('live/new_comment');
 			?>
 			
-<form method=post="http://www.kumquat.com/demo">
-<input type="hidden" name="video_id" value="<?=$video_id?>"/>
-<table><tr><td>
+		<input type="hidden" name="video_id" value="<?=$video_id?>"/>
+		<table><tr><td>
+		<? 		if(isset($username) == False) { ?>
+			
 ÇÓד ÇבדÓÊÎÏד:</td><td><input type="text" name="username"/></td></tr><tr><td>
 ÇבÈÑםÏ ÇבÇב‗ÊÑזהל:</td><td><input type="text" name="email"/></td></tr><tr><td>
+<?}?>
 ÇבÊÚבםÞ Úבל ÇבÝÏםז:</td><td>
 <textarea name="text" cols=40 rows=4>
 </textarea></td></tr><tr><td></td><td>
-<input type=submit value="ÇÑÓב"></td></tr></table>
+<input type=submit class="button_01"value="ÇÑÓב"></td></tr></table>
 </form>
+
