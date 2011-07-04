39d2214cc7b5ae6a6ee5bb0b208626c0d6cc3a3e
Ahmed Essam <eng.eramax@gmail.com>
2011-07-04 14:50:07 +0300

    publisher running well and we can start broadcast and subcribe as well RTMP server ip is 172.17.17.102

--- application/views/comment_form.php
+++ application/views/comment_form.php
@@ -1,31 +1,31 @@
 <?php
 $ccc = '</br></br>';
 foreach ($comments as $comment) { ?>
-	<div class="comx">
-	<ul class="comment">
-	<li id="comment_2520527" class="clearfix comment comment_2520527 userid_167224057">
-	 <span> 
-	 <div id="2520527" class="avatar female offline">
-	 <a href="/nor201104" title="nor201104 | ??? ?????? | ???? | 20 ???/?????" class="imageWrap person">
-	  <img src="<?echo base_url() . 'img/thumb_male.jpg' ;?>" alt="" width="80" height="60">
-	 </a>
-	 <a href="/nor201104" title="nor201104 | ??? ?????? | ???? | 20 ???/?????" class="nick person">
-	  <span><?=$comment->username?></span> </a>
-	<span class="age">20</span></div>
-	 </span>
-	 <p> 
-	  
-	 <a href="/nor201104" class="displayName"><?= $comment->username?></a>
-		  <small class="dimmed"> <?=$comment->date?> </small>
-	 </p>
-	 <p class="body smiley">
-	 <?=$comment->text?> 
-	 
-	 </p>
-	  <hr>
-	  </li>
-	  </ul>
-	</div>
+<div class="comx">
+<ul class="comment">
+<li id="comment_2520527" class="clearfix comment comment_2520527 userid_167224057">
+ <span> 
+ <div id="2520527" class="avatar female offline">
+ <a href="/nor201104" title="nor201104 | ??? ?????? | ???? | 20 ???/?????" class="imageWrap person">
+  <img src="http://localhost/ci/img/thumb_male.jpg" alt="" width="80" height="60">
+ </a>
+ <a href="/nor201104" title="nor201104 | ??? ?????? | ???? | 20 ???/?????" class="nick person">
+  <span><?=$comment->username?></span> </a>
+<span class="age">20</span></div>
+ </span>
+ <p> 
+  
+ <a href="/nor201104" class="displayName"><?=$comment->username?></a>
+      <small class="dimmed"><?=$comment->date?></small>
+ </p>
+ <span class="body smiley">
+ 
+<?=$comment->text?></span>
+  <hr>
+  </li>
+  </ul>
+  </div>
+
 
 <?}?>
 
@@ -37,7 +37,11 @@ foreach ($comments as $comment) { ?>
 		</br>
 	  </br>
 	  </br>		</br>
-	  </br>	  </br>		</br>
+	  </br>
+	  </br>		</br>
+	  </br>
+	  </br>		</br>
+	  </br>
 	  </br>
 		<input type="hidden" name="video_id" value="<?=$video_id?>"/>
 		<table><tr><td>
